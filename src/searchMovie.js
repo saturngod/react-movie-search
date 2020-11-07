1469666c5b7dced0d5b6b82e3408020e03030e89
Saturngod <saturngod@gmail.com>
2020-11-08 01:50:13 +0630

    add pagination without page changes

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -7,6 +7,7 @@ import ResultContext from './RestContext';
 function SearchMovie() {
    
     const [query,setQuery] = useState('');
+    const [page,setPage] = useState(1);
     const [movies, setMovies] = useState([]);
     const [loading,setLoading] = useState(false);
 
@@ -14,20 +15,37 @@ function SearchMovie() {
 
     const searchMovies = async(e) => {
         e.preventDefault();
-        console.log("submitting");
+        setPage(1)
+        movieSearching(query,page)
+    }
+
+    const next = () => {
+        setPage(page+1)
+        movieSearching(query,page)
+    }
+
+    const prev = () => {
+        if (page > 1) {
+            setPage(page-1)
+            movieSearching(query,page)
+        }
+        
+    }
+
+    const movieSearching = async(q,p) => {
+
         setLoading(true);
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=a92f28e11a27e8e5938a2020be68ba9c&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=a92f28e11a27e8e5938a2020be68ba9c&language=en-US&query=${q}&page=${p}&include_adult=false`;
 
         try {
             const res = await fetch(url);
             const data = await res.json();
             setMovies(data.results);
             setLoading(false);
-            updateCount(data.results.length)
+            updateCount(data.total_results)
         } catch (err) {
             console.error(err);
         }
-        
     }
 
     return (
@@ -40,8 +58,15 @@ function SearchMovie() {
                     placeholder="i.e. Jurassic Park"/>
                 <button className="button" type="submit">Search</button>
             </form>
+            <div className="showingPage">
+                Current Page is {page}
+            </div>
             <Loading isLoading={loading}></Loading>
             <MovieList movies={movies}></MovieList>
+            <ul className="pagination">
+                <li onClick={prev}><a href="#">Prev</a></li>
+                <li onClick={next}><a href="#">Next</a></li>
+            </ul>
 
         </div>
       
